79f52e0cb3b4cf565229f684482640d8f7a2859a
hanawtnb <hanawtnb@icloud.com>
2022-01-26 11:06:53 +0900

    Dashboradを作成

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import { Body } from "./Body";
+// import { Right } from "./Right";
+import { Sidebar } from "./Sidebar";
+
+export const Dashboard = () => {
+  return (
+    <main>
+      <Sidebar />
+      <Body />
+      {/* <Right /> */}
+    </main>
+  );
+};
