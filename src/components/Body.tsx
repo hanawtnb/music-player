18782c4a6efa6ca549eb12353525805b3e8c3c43
hanawtnb <hanawtnb@icloud.com>
2022-01-27 15:44:02 +0900

    新着音楽の取得と表示の実装

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,5 +1,6 @@
 import { useSession } from "next-auth/react";
 import { memo, useEffect, useState, VFC } from "react";
+import Poster from "./Poster";
 import { Search } from "./Searchbar";
 
 type Props = {
@@ -43,12 +44,51 @@ export const Body: VFC<Props> = (props) => {
     });
   }, [search, accessToken, spotifyApi]);
 
-  console.log(searchResults);
+  //新着
+  useEffect(() => {
+    if (!accessToken) return;
+
+    spotifyApi.getNewReleases().then((res: any) => {
+      // console.log(res);
+
+      setNewRelease(
+        res.body.albums.items.map((track: any) => {
+          return {
+            id: track.id,
+            artist: track.artists[0].name,
+            title: track.name,
+            //音楽を再生するために使う。
+            uri: track.uri,
+            albumUrl: track.images[0].url,
+            // popularity: track.popularity,
+          };
+        })
+      );
+    });
+  }, [search, accessToken]);
+
+  console.log(newRelease);
 
   return (
     <section className="bg-black ml-60 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
       <Search search={search} setSearch={setSearch} />
-      <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4"></div>
+      <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4">
+        {searchResults.length === 0
+          ? newRelease.slice(0, 4).map((track: any) => (
+              <Poster
+                key={track.id}
+                track={track}
+                // chooseTrack={chooseTrack}
+              />
+            ))
+          : searchResults.slice(0, 4).map((track: any) => (
+              <Poster
+                key={track.id}
+                track={track}
+                // chooseTrack={chooseTrack}
+              />
+            ))}
+      </div>
     </section>
   );
 };
