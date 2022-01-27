249efdd24071ff577c828cf5cf4566b3434b1358
hanawtnb <hanawtnb@icloud.com>
2022-01-27 17:39:14 +0900

    PrimaryButtonコンポーネントを追加

---
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -0,0 +1,16 @@
+import { memo, ReactNode, VFC } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const PrimaryButton: VFC<Props> = (props) => {
+  const { children } = props;
+  return (
+    <button className="text-[#CECECE] bg-[#1A1A1A] text-[13px] py-3.5 px-4 rounded-2xl w-full font-bold bg-opacity-80 hover:bg-opacity-100 transition ease-out">
+      {children}
+    </button>
+  );
+};
+
+export default memo(PrimaryButton);
