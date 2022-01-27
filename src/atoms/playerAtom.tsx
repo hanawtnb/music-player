a635c12e8e2e76fc5b21e12bf40c6246ff901373
hanawtnb <hanawtnb@icloud.com>
2022-01-27 16:33:57 +0900

    再生に関するグローバルステートの設定

---
+++ src/atoms/playerAtom.tsx
@@ -0,0 +1,14 @@
+import { atom } from "recoil";
+
+// 再生のステータス
+// const [play,setPlay] =useState(false)と同じこと
+export const playState = atom({
+  key: "playState",
+  default: false,
+});
+
+// 再生中の曲
+export const playingTrackState = atom({
+  key: "playingTrackState",
+  default: "",
+});
