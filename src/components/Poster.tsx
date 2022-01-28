2c6730152a2fe38cfb9c34571069ae68119adc23
hanawtnb <hanawtnb@icloud.com>
2022-01-28 15:18:00 +0900

    エラーを修正

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { memo, VFC } from "react";
+import { memo, useCallback, VFC } from "react";
 import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
 import { useRecoilState } from "recoil";
 import { playingTrackState, playState } from "../atoms/playerAtom";
@@ -15,7 +15,6 @@ const Poster: VFC<Props> = (props) => {
   const [playingTrack, setPlayingTrack] = useRecoilState(
     playingTrackState
   ) as any;
-
   const onClickPlayMusic = () => {
     chooseTrack(track);
 
@@ -23,7 +22,6 @@ const Poster: VFC<Props> = (props) => {
       setPlay(!play);
     }
   };
-
   return (
     <div
       onClick={onClickPlayMusic}
@@ -36,17 +34,17 @@ const Poster: VFC<Props> = (props) => {
       />
       <div className="absolute bottom-10 inset-x-0 ml-4 flex items-center space-x-3.5">
         <div className="h-10 w-10 bg-[#15883e] rounded-full flex items-center justify-center group-hover:bg-[#1db954] flex-shrink-0">
-          {track.uri === playingTrack.uri && play ? (
+          {track?.uri === playingTrack?.uri && play ? (
             <BsFillPauseFill className="text-xl " />
           ) : (
             <BsFillPlayFill className="text-xl ml-[1px]" />
           )}
         </div>
         <div className="text-[15px]">
-          <h4 className="font-extrabold truncate w-44 line-clamp-{n}">
+          <h4 className="font-extrabold truncate w-44 line-clamp-{1}">
             {track.title}
           </h4>
-          <h6>{track.artist}</h6>
+          <h6 className="line-clamp-{1}">{track?.artist}</h6>
         </div>
       </div>
     </div>
