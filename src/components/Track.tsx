e5e4143f74f86ccdb7615df5c97a4bd3e5c1f18b
hanawtnb <hanawtnb@icloud.com>
2022-01-28 15:18:20 +0900

    曲の再生機能を実装

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -1,6 +1,10 @@
-import { memo, VFC } from "react";
+import { memo, useState, VFC } from "react";
 import { ImHeadphones } from "react-icons/im";
 import { AiFillHeart } from "react-icons/ai";
+import { useRecoilState } from "recoil";
+
+import { playingTrackState, playState } from "../atoms/playerAtom";
+import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
 
 type Props = {
   track: any;
@@ -9,7 +13,17 @@ type Props = {
 
 const Track: VFC<Props> = (props) => {
   const { track, chooseTrack } = props;
-  console.log("トラック", track);
+  const [hasLiked, setHasLiked] = useState(false);
+  const [play, setPlay] = useRecoilState(playState);
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+
+  const onClickPlayMusic = () => {
+    chooseTrack(track);
+
+    if (track?.uri === playingTrack?.uri) {
+      setPlay(!play);
+    }
+  };
 
   return (
     <div className="flex items-center justify-between space-x-20 cursor-default hover:bg-white/10 py-2 px-4 rounded-lg group transition ease-out">
@@ -26,6 +40,36 @@ const Track: VFC<Props> = (props) => {
           <p className="text-white/70 text-[13px] font-semibold group-hover:text-white">
             {track.artist}
           </p>
+        </div>
+      </div>
+
+      <div className="md:ml-auto flex items-center space-x-2.5">
+        <div className="text-white flex space-x-1 text-sm font-semibold">
+          <ImHeadphones className="text-lg" />
+          <h4>{track.popularity}</h4>
+        </div>
+        <div className="flex items-center rounded-full border-2 border-[#262626] w-[85px] h-10 relative cursor-pointer group-hover:border-white/40">
+          <AiFillHeart
+            className={`text-xl ml-3 icon ${
+              hasLiked ? "text-[#1ed760]" : "text-[#868686]"
+            }`}
+            onClick={() => setHasLiked(!hasLiked)}
+          />
+          {track.uri === playingTrack?.uri && play ? (
+            <div
+              className="h-10 w-10 rounded-full border border-[#15883e] flex items-center justify-center absolute -right-0.5 bg-[#15883e] icon hover:scale-110"
+              onClick={onClickPlayMusic}
+            >
+              <BsFillPauseFill className="text-white text-xl" />
+            </div>
+          ) : (
+            <div
+              className="h-10 w-10 rounded-full border border-white/60 flex items-center justify-center absolute -right-0.5 hover:bg-[#15883e] hover:border-[#15883e] icon hover:scale-110"
+              onClick={onClickPlayMusic}
+            >
+              <BsFillPlayFill className="text-white text-xl ml-[1px]" />
+            </div>
+          )}
         </div>
       </div>
     </div>
