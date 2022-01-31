e6eada325f90375c60d7c9a3d2df42a8d7b9ac83
hanawtnb <hanawtnb@icloud.com>
2022-01-31 11:16:35 +0900

    Playerを追加

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,18 +1,30 @@
 import { useRecoilState } from "recoil";
 import SpotifyWebApi from "spotify-web-api-node";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 import { playingTrackState } from "../atoms/playerAtom";
-
 import { Sidebar } from "./Sidebar";
 import { Body } from "./Body";
 import { Right } from "./Right";
+import { Player } from "./Player";
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
 });
 
 export const Dashboard = () => {
+  //playerを表示させるために必要
+  const { data: session } = useSession();
+  const accessToken = session?.accessToken;
+
   const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  useEffect(() => {
+    setShowPlayer(true);
+  }, []);
+
   const chooseTrack = (track: any): any => {
     setPlayingTrack(track);
   };
@@ -22,6 +34,9 @@ export const Dashboard = () => {
       <Sidebar />
       <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
       <Right spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
+      <div className="fixed bottom-0 left-0 right-0 z-50 ">
+        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+      </div>
     </main>
   );
 };
---
+++ src/components/Player.tsx
@@ -0,0 +1,53 @@
+import SpotifyPlayer from "react-spotify-web-playback";
+import { useEffect, VFC } from "react";
+import { useRecoilState } from "recoil";
+
+import { playingTrackState, playState } from "../atoms/playerAtom";
+
+type Props = {
+  accessToken: any;
+  trackUri: string;
+};
+
+export const Player: VFC<Props> = (props) => {
+  const { accessToken, trackUri } = props;
+  const [play, setPlay] = useRecoilState(playState);
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+
+  useEffect(() => {
+    if (trackUri) {
+      setPlay(true);
+    }
+  }, [setPlay, trackUri]);
+
+  if (!accessToken) return null;
+  return (
+    <div>
+      <SpotifyPlayer
+        styles={{
+          activeColor: "#fff",
+          bgColor: "#181818",
+          color: "#fff",
+          loaderColor: "#fff",
+          sliderColor: "#1cb954",
+          trackArtistColor: "#ccc",
+          trackNameColor: "#fff",
+          height: "70px",
+          sliderTrackColor: "#535353",
+          sliderTrackBorderRadius: "4px",
+          sliderHandleColor: "#fff",
+          errorColor: "#fff",
+        }}
+        token={accessToken}
+        showSaveIcon
+        callback={(state) => {
+          setPlay(state.isPlaying);
+        }}
+        play={play}
+        uris={trackUri ? [trackUri] : []}
+        magnifySliderOnHover={true}
+        autoPlay={true}
+      />
+    </div>
+  );
+};
