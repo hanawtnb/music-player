ad0a389485afd2836a038c357df33c1497a1c3b1
hanawtnb <hanawtnb@icloud.com>
2022-01-31 15:39:12 +0900

    再生ボタンの範囲を拡大

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -26,21 +26,24 @@ const Track: VFC<Props> = (props) => {
   };
 
   return (
-    <div className="flex items-center justify-between space-x-20 cursor-default hover:bg-white/10 py-2 px-4 rounded-lg group transition ease-out">
-      <div className="flex items-center">
+    <div className=" flex items-center justify-between space-x-20 cursor-default hover:bg-white/10 py-2 px-4 rounded-lg group transition ease-out">
+      <div
+        className="items-center cursor-pointer w-full"
+        onClick={onClickPlayMusic}
+      >
         <img
           src={track.albumUrl}
           alt=""
-          className="rounded-xl h-12 w-12 object-cover mr-3"
+          className="rounded-xl h-12 w-12 object-cover mr-3 float-left"
         />
-        <div>
+        <span>
           <h4 className="text-white text-sm font-semibold truncate w-[450px]">
             {track.title}
           </h4>
           <p className="text-white/70 text-[13px] font-semibold group-hover:text-white">
             {track.artist}
           </p>
-        </div>
+        </span>
       </div>
 
       <div className="md:ml-auto flex items-center space-x-2.5">
@@ -56,17 +59,11 @@ const Track: VFC<Props> = (props) => {
             onClick={() => setHasLiked(!hasLiked)}
           />
           {track.uri === playingTrack?.uri && play ? (
-            <div
-              className="h-10 w-10 rounded-full border border-[#15883e] flex items-center justify-center absolute -right-0.5 bg-[#15883e] icon hover:scale-110"
-              onClick={onClickPlayMusic}
-            >
+            <div className="h-10 w-10 rounded-full border border-[#15883e] flex items-center justify-center absolute -right-0.5 bg-[#15883e] icon hover:scale-110">
               <BsFillPauseFill className="text-white text-xl" />
             </div>
           ) : (
-            <div
-              className="h-10 w-10 rounded-full border border-white/60 flex items-center justify-center absolute -right-0.5 hover:bg-[#15883e] hover:border-[#15883e] icon hover:scale-110"
-              onClick={onClickPlayMusic}
-            >
+            <div className="h-10 w-10 rounded-full border border-white/60 flex items-center justify-center absolute -right-0.5 hover:bg-[#15883e] hover:border-[#15883e] icon hover:scale-110">
               <BsFillPlayFill className="text-white text-xl ml-[1px]" />
             </div>
           )}
