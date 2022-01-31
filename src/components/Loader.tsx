07c7f0bc50a0049b30616dbe8d4279eb13e75922
hanawtnb <hanawtnb@icloud.com>
2022-01-31 13:55:18 +0900

    Loadingを修正

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,3 +1,21 @@
+import ReactLoading from "react-loading";
+import Image from "next/image";
+
 export const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="h-screen bg-black">
+      <div className="pt-40 flex flex-col items-center space-y-4">
+        <span>
+          <Image
+            src="/spotifylogo.png"
+            height={250}
+            width={600}
+            objectFit="contain"
+            className="animate-pulse"
+          />
+        </span>
+        <ReactLoading type="bars" color="white" />
+      </div>
+    </div>
+  );
 };
