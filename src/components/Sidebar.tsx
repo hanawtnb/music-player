e4d22d10e01ce206eb0f7609e1f57a619c35219f
hanawtnb <hanawtnb@icloud.com>
2022-02-02 13:25:17 +0900

    Playlistページの表示を実装

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,8 +7,16 @@ import {
 import { FaMicrophoneAlt } from "react-icons/fa";
 import { RiCompassFill } from "react-icons/ri";
 import Image from "next/image";
+import router from "next/router";
+import { useRecoilState } from "recoil";
+
+import { playlistSelectedState } from "../atoms/playerAtom";
 
 export const Sidebar = () => {
+  const [playlistSelected, setPlaylistSelected] = useRecoilState(
+    playlistSelectedState
+  );
+
   return (
     <section className="fixed top-0 left-0 z-40 flex flex-col p-4 items-center bg-black w-[225px] h-screen space-y-8">
       <Image src="/logo.png" width={56} height={56} objectFit="contain" />
@@ -25,9 +33,15 @@ export const Sidebar = () => {
           <ChartBarIcon className="sidebarIcon" />
           <span className="text-white">Chart</span>
         </button>
-        <button className="flex justify-center space-x-5">
+        <button
+          type="button"
+          className="flex justify-center space-x-5"
+          onClick={() => {
+            setPlaylistSelected(true);
+          }}
+        >
           <ClockIcon className="sidebarIcon" />
-          <span className="text-white">History</span>
+          <span className="text-white">Playlists</span>
         </button>
         <button className="flex justify-center space-x-5">
           <DotsHorizontalIcon className="sidebarIcon" />
