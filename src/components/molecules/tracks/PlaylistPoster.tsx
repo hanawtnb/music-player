c270aadaa514f60b0eddc6bc3b5c91fec9b7bfb5
hanawtnb <hanawtnb@icloud.com>
2022-02-03 15:52:45 +0900

    imageがない時のデフォルトimageを設定

--- src/components/molecules/tracks/PlaylistPoster.tsx
+++ src/components/molecules/tracks/PlaylistPoster.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { memo, useCallback, VFC } from "react";
 import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
+import { HiOutlineMusicNote } from "react-icons/hi";
 import { useRecoilState } from "recoil";
 import { playingTrackState, playState } from "../../../atoms/playerAtom";
 
@@ -23,11 +24,16 @@ const Poster: VFC<Props> = (props) => {
           // onClick={onClickPlayMusic}
           className="bg-[#0D0D0D] w-[260px] h-[260px] rounded-[50px] overflow-hidden relative cursor-pointer hover:scale-105 hover:text-white/100 transition duration-200 ease-out group mx-auto"
         >
-          <img
-            src={playlist?.image}
-            alt=""
-            className="h-full w-full object-cover rounded-[50px] opacity-80 group-hover:opacity-100"
-          />
+          {playlist?.image ? (
+            <img
+              src={playlist?.image}
+              alt=""
+              className="h-full w-full object-cover rounded-[50px] opacity-80 group-hover:opacity-100"
+            />
+          ) : (
+            <HiOutlineMusicNote className="text-white/80 text-7xl absolute top-24 left-24 items-center justify-center" />
+          )}
+
           <div className="absolute bottom-8 left-44 inset-x-0 ml-4 flex items-center space-x-3.5">
             <div className="text-white/80 h-10 w-10 bg-[#15883e] rounded-full flex items-center justify-center group-hover:bg-[#1db954] flex-shrink-0">
               {playlist?.uri === playingTrack?.uri && play ? (
