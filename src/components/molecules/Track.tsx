57059cc839211dc7ba2b5ba4065dab7e6028ba01
hanawtnb <hanawtnb@icloud.com>
2022-02-07 16:12:32 +0900

    お気に入り追加機能を実装

--- src/components/molecules/Track.tsx
+++ src/components/molecules/Track.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState, VFC } from "react";
-import { ImHeadphones } from "react-icons/im";
 import { AiFillHeart } from "react-icons/ai";
 import { useRecoilState } from "recoil";
 
@@ -38,11 +37,22 @@ const Track: VFC<Props> = (props: Props) => {
 
   const addPlayFavorite = (index: string, title: any): void => {
     // if (!accessToken) return;
-    setHasLiked(!hasLiked);
+    setHasLiked(true);
     spotifyApi.getAlbumTracks([index]).then((res: any) => {
       res.body.items.map((track: any) => {
         if (track.name === title) {
           return spotifyApi.addToMySavedTracks([track.id]);
+        }
+      });
+    });
+  };
+
+  const removePlayFavorite = (index: string, title: any) => {
+    setHasLiked(false);
+    spotifyApi.getAlbumTracks([index]).then((res: any) => {
+      res.body.items.map((track: any) => {
+        if (track.name === title) {
+          return spotifyApi.removeFromMySavedTracks([track.id]);
         }
       });
     });
@@ -72,23 +82,26 @@ const Track: VFC<Props> = (props: Props) => {
       </div>
 
       <div className="md:ml-auto flex items-center space-x-2.5">
-        <div className="text-white flex space-x-1 text-sm font-semibold">
-          <ImHeadphones className="text-lg" />
-          <h4>{track.popularity}</h4>
-        </div>
         <div className="flex items-center rounded-full border-2 border-[#262626] w-[85px] h-10 relative cursor-pointer group-hover:border-white/40">
           <AiFillHeart
             className={`text-xl ml-3 icon ${
               hasLiked ? "text-[#1ed760]" : "text-[#868686]"
             }`}
-            onClick={() => addPlayFavorite(track.id, track.title)}
+            onClick={
+              hasLiked
+                ? () => removePlayFavorite(track.id, track.title) as any
+                : () => addPlayFavorite(track.id, track.title) as any
+            }
           />
           {track.uri === playingTrack?.uri && play ? (
             <div className="h-10 w-10 rounded-full border border-[#15883e] flex items-center justify-center absolute -right-0.5 bg-[#15883e] icon hover:scale-110">
               <BsFillPauseFill className="text-white text-xl" />
             </div>
           ) : (
-            <div className="h-10 w-10 rounded-full border border-white/60 flex items-center justify-center absolute -right-0.5 hover:bg-[#15883e] hover:border-[#15883e] icon hover:scale-110">
+            <div
+              onClick={onClickPlayMusic}
+              className="h-10 w-10 rounded-full border border-white/60 flex items-center justify-center absolute -right-0.5 hover:bg-[#15883e] hover:border-[#15883e] icon hover:scale-110"
+            >
               <BsFillPlayFill className="text-white text-xl ml-[1px]" />
             </div>
           )}
