2eaac76461c4bc5d562c03316229c059dd680400
hanawtnb <hanawtnb@icloud.com>
2022-02-14 10:19:30 +0900

    不要なインポートを削除

--- src/pages/playlists.tsx
+++ src/pages/playlists.tsx
@@ -1,13 +1,8 @@
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import SpotifyWebApi from "spotify-web-api-node";
 import { playingTrackState } from "../atoms/playerAtom";
-import { Body } from "../components/template/Body";
-import { Player } from "../components/organisms/layout/Player";
 import { AllPlaylist } from "../components/template/AllPlaylist";
-import { Right } from "../components/organisms/layout/Right";
-import { Sidebar } from "../components/organisms/layout/Sidebar";
 import { SidebarLayout } from "../components/template/SidebarLayout";
 
 const spotifyApi = new SpotifyWebApi({
